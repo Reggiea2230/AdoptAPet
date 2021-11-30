9de91793b06e9e637a7be84e8295b8d50525868f
Reggie <reginaldallonce@gmail.com>
2021-11-29 22:51:57 -0500

    adding api

--- config/database.js
+++ config/database.js
@@ -1,18 +1,15 @@
-
-  
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-  'mongodb://localhost:27017/testagram',
-  { useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  }
-);
+  process.env.DATABASE_URL, // < replace with your database name!
+)
 
 const db = mongoose.connection;
 
-db.on('connected', function() {
+db.on("connected", function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
+
+db.on('error', function(err){
+  console.log(`Mongodb error: ${err}`)
+})
